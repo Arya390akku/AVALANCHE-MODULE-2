eea6e8fb8b606f0e978ab12ee83b358807f08187
Arya390akku <aryaofficial390@gmail.com>
2024-08-09 00:41:12 +0530

    Update Aryhaakku.sol

--- Aryhaakku.sol
+++ Aryhaakku.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Aryhakku is ERC20, Ownable {
+contract token is ERC20, Ownable {
     constructor(address initialOwner)
         ERC20("SPECIALSO", "SPO")
         Ownable(initialOwner)
